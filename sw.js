e940876cc30216ac421871041c7e8dacaaf8b549
azatdron <azatrafikov85@gmail.com>
2026-06-02 18:42:48 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='cargo-diary-v9';
+const CACHE='cargo-diary-v10-v10';
 const ASSETS=['./index.html?v=7','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).catch(()=>{}))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
